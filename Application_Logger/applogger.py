1513f346605940ff639cde85a4506928c5c52d23
rangaomkaram <omkaram.ranga@gmail.com>
2022-05-07 20:10:01 +0530

    applogger

---
+++ Application_Logger/applogger.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+class App_Logger:
+    """
+    This class is used for log messages of files and return timestamp 
+    of the file
+
+    written By : Omkaram Rangasesha
+    
+    """
+    def __init__(self,object_files,log_messages):
+
+        """
+        This intialization constructor used to create the instance of the objects and class members 
+        """
