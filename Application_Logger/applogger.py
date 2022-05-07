edfefc77922488ab9aab662341b8e0b1ecb47103
rangaomkaram <omkaram.ranga@gmail.com>
2022-05-07 20:28:43 +0530

    application logging python

--- Application_Logger/applogger.py
+++ Application_Logger/applogger.py
@@ -8,8 +8,16 @@ class App_Logger:
     written By : Omkaram Rangasesha
     
     """
-    def __init__(self,object_files,log_messages):
+    def __init__(self):
+        pass
+
+    def logging_msg(self,object_files,log_messages):
 
         """
         This intialization constructor used to create the instance of the objects and class members 
         """
+        self.now = datetime.now()
+        self.date = self.now.date()
+        self.current_time = self.now.strftime("%H:%M:%S")
+        object_files.write(str(self.data) + "/" + str(self.current_time) + "\t\t"+log_messages + "\n")
+
